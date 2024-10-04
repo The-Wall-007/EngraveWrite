84c66b443a67a2c43e6e1852a3470a091e2a0533
The-Wall-007 <dvlpatel2410@gmail.com>
2024-10-04 20:13:48 +0530

    Added Signage Page

---
+++ engrave-write/src/app/Signage/page.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+
+import Header from "@/components/Header";
+import ServicesDone from "@/components/Signage/ServicesDone";
+
+const Signage = () => {
+  return (
+    <React.Fragment>
+      <Header />
+
+      <Stack
+        sx={{
+          backgroundColor: "#EBEBF5",
+          padding: {
+            xs: 4,
+            sm: 6,
+            md: 8,
+            lg: 12,
+          },
+        }}
+      >
+        <Stack
+          sx={{
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 1,
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "50px",
+              fontWeight: "500",
+              color: "#2C2C2C",
+              lineHeight: "54px",
+              fontFamily: "rubik",
+              textAlign: "center",
+            }}
+          >
+            Signage Solution
+          </Typography>
+
+          <Stack
+            sx={{
+              marginTop: 2.5,
+              height: 8,
+              width: 96,
+              backgroundColor: "#FF3B99",
+              borderRadius: 2,
+            }}
+          />
+        </Stack>
+      </Stack>
+
+      <ServicesDone />
+    </React.Fragment>
+  );
+};
+
+export default Signage;
